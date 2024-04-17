c6648524103dcc28421e1f047541dea7a7561ae5
rookedsysc <rookedsysc36@gmail.com>
2024-04-17 14:11:36 +0900

    feat Nextra Documentation 추가

---
+++ Documentation/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <link rel="shortcut icon" href="/public/cs-log-logo.png"/>
+            <Head/>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
+}
---
+++ Documentation/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>CS Study</span>,
+  project: {
+    link: 'https://github.com/jmxx219/CS-Study',
+  },
+  chat: {
+    link: 'https://discord.gg/ZQnksThw',
+  },
+  docsRepositoryBase: 'https://github.com/jmxx219/CS-Study',
+  footer: {
+    text: 'CS STUDY PLACE',
+  },
+}
+
+export default config
